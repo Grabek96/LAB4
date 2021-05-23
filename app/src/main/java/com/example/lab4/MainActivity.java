d1310f43fb1be4d15ebffa4e78cf1efaea043198
Grabek96 <kgrabowski11@wp.pl>
2021-05-23 15:53:43 +0200

    ostatni commit

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lab4;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -39,4 +40,18 @@ public class MainActivity extends AppCompatActivity {
         Intent intencja = new Intent ( this, DodajWpis.class);
         startActivityForResult(intencja, 1);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK){
+
+            Bundle extras = data.getExtras();
+            String nowy = (String) extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+
+        }
+    }
+
 }
